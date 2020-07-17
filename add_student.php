dbe7801c4fb237ab40b33db855b0fbab7c51cea1
vedantj25 <vjjc25@gmail.com>
2020-07-17 15:26:27 +0530

    x

--- add_student.php
+++ add_student.php
@@ -50,6 +50,49 @@
   <link href="css/sb-admin-2.css" rel="stylesheet">
 
 </head>
+<?php
+if($_POST['submit'])
+{
+$roomno=$_POST['room'];
+$seater=$_POST['seater'];
+$feespm=$_POST['fpm'];
+$foodstatus=$_POST['foodstatus'];
+$stayfrom=$_POST['stayf'];
+$duration=$_POST['duration'];
+$course=$_POST['course'];
+$regno=$_POST['regno'];
+$fname=$_POST['fname'];
+$mname=$_POST['mname'];
+$lname=$_POST['lname'];
+$gender=$_POST['gender'];
+$contactno=$_POST['contact'];
+$emailid=$_POST['email'];
+$emcntno=$_POST['econtact'];
+$gurname=$_POST['gname'];
+$gurrelation=$_POST['grelation'];
+$gurcntno=$_POST['gcontact'];
+$caddress=$_POST['address'];
+$ccity=$_POST['city'];
+$cstate=$_POST['state'];
+$cpincode=$_POST['pincode'];
+$paddress=$_POST['paddress'];
+$pcity=$_POST['pcity'];
+$pstate=$_POST['pstate'];
+$ppincode=$_POST['ppincode'];
+
+$query="insert into registration(roomno,seater,feespm,foodstatus,stayfrom,duration,course,regno,firstName,middleName,lastName,gender,contactno,emailid,egycontactno,guardianName,guardianRelation,guardianContactno,corresAddress,corresCIty,corresState,corresPincode,pmntAddress,pmntCity,pmnatetState,pmntPincode)
+  values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+$stmt = $mysqli->prepare($query);
+$rc=$stmt->bind_param('iiiisisissssisississsisssi',$roomno,$seater,$feespm,$foodstatus,$stayfrom,$duration,$course,$regno,$fname,$mname,$lname,$gender,$contactno,$emailid,$emcntno,$gurname,$gurrelation,$gurcntno,$caddress,$ccity,$cstate,$cpincode,$paddress,$pcity,$pstate,$ppincode);
+$stmt->execute();
+$stmt->close();
+
+}
+?>
+
+
+
+
 
 <body>
 	<body id="page-top">
@@ -66,7 +109,7 @@
 	          </div>
 
 	          <!-- Content Row -->
-	          <div class="row">
+
 
 	            <!-- Earnings (Monthly) Card Example -->
 
@@ -74,264 +117,259 @@
 
 	          <!-- Content Row -->
 
-
-              <form method="post" action="" class="form-horizontal">
-
-
-    <div class="form-group">
-    <label class="col-lg-12 control-label"><h4 style="color: green" align="left">Room Related info </h4> </label>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Room no. </label>
-    <div class="col-lg-8">
-    <select name="room" id="room"class="form-control"  onChange="getSeater(this.value);" onBlur="checkAvailability()" required>
-    <option value="">Select Room</option>
-
-    </select>
-    <span id="room-availability-status" style="font-size:12px;"></span>
-
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Seater</label>
-    <div class="col-lg-8">
-    <input type="text" name="seater" id="seater"  class="form-control"  >
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Fees Per Month</label>
-    <div class="col-lg-8">
-    <input type="text" name="fpm" id="fpm"  class="form-control" >
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Food Status</label>
-    <div class="col-lg-8">
-    <input type="radio" value="0" name="foodstatus" checked="checked"> Without Food
-    <input type="radio" value="1" name="foodstatus"> With Food(Rs 2000.00 Per Month Extra)
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Stay From</label>
-    <div class="col-lg-8">
-    <input type="date" name="stayf" id="stayf"  class="form-control" >
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Duration</label>
-    <div class="col-lg-8">
-    <select name="duration" id="duration" class="form-control">
-    <option value="">Select Duration in Month</option>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-    <option value="9">9</option>
-    <option value="10">10</option>
-    <option value="11">11</option>
-    <option value="12">12</option>
-    </select>
-    </div>
-    </div>
+<div>
+              <form method="post" action="add_student.php" class="form-horizontal">
 
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label"><h4 style="color: green" align="left">Personal info </h4> </label>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-12 control-label"><h4 style="color: green" align="left">Room Related info </h4> </label>
+  </div>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">course </label>
-    <div class="col-lg-8">
-    <select name="course" id="course" class="form-control" required>
-    <option value="">Select Course</option>
+  <div class="form-group">
+  <label class="col-lg-6 control-label">Room no.:</label>
+  <div class="col-lg-6">
+  <select name="room" id="room"class="form-control"  onChange="getSeater(this.value);" onBlur="checkAvailability()" required>
+  <option value="">Select Room</option>
 
-    </select> </div>
-    </div>
+  </select>
+  <span id="room-availability-status" style="font-size:12px;"></span>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Registration No : </label>
-    <div class="col-lg-8">
-    <input type="text" name="regno" id="regno"  class="form-control" required="required" >
-    </div>
-    </div>
+  </div>
+  </div>
 
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Seater</label>
+  <div class="col-lg-6">
+  <input type="text" name="seater" id="seater"  class="form-control"  >
+  </div>
+  </div>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">First Name : </label>
-    <div class="col-lg-8">
-    <input type="text" name="fname" id="fname"  class="form-control" required="required" >
-    </div>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Fees Per Month</label>
+  <div class="col-lg-6">
+  <input type="text" name="fpm" id="fpm"  class="form-control" >
+  </div>
+  </div>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Middle Name : </label>
-    <div class="col-lg-8">
-    <input type="text" name="mname" id="mname"  class="form-control">
-    </div>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Food Status</label>
+  <div class="col-lg-6">
+  <input type="radio" value="0" name="foodstatus" checked="checked"> Without Food
+  <input type="radio" value="1" name="foodstatus"> With Food(Rs 2000.00 Per Month Extra)
+  </div>
+  </div>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Last Name : </label>
-    <div class="col-lg-8">
-    <input type="text" name="lname" id="lname"  class="form-control" required="required">
-    </div>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Stay From</label>
+  <div class="col-lg-6">
+  <input type="date" name="stayf" id="stayf"  class="form-control" >
+  </div>
+  </div>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Gender : </label>
-    <div class="col-lg-8">
-    <select name="gender" class="form-control" required="required">
-    <option value="">Select Gender</option>
-    <option value="male">Male</option>
-    <option value="female">Female</option>
-    <option value="others">Others</option>
-    </select>
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Contact No : </label>
-    <div class="col-lg-8">
-    <input type="text" name="contact" id="contact"  class="form-control" required="required">
-    </div>
-    </div>
-
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Email id : </label>
-    <div class="col-lg-8">
-    <input type="email" name="email" id="email"  class="form-control" required="required">
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Emergency Contact: </label>
-    <div class="col-lg-8">
-    <input type="text" name="econtact" id="econtact"  class="form-control" required="required">
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Guardian  Name : </label>
-    <div class="col-lg-8">
-    <input type="text" name="gname" id="gname"  class="form-control" required="required">
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Guardian  Relation : </label>
-    <div class="col-lg-8">
-    <input type="text" name="grelation" id="grelation"  class="form-control" required="required">
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Guardian Contact no : </label>
-    <div class="col-lg-8">
-    <input type="text" name="gcontact" id="gcontact"  class="form-control" required="required">
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-sm-3 control-label"><h4 style="color: green" align="left">Correspondense Address </h4> </label>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Duration</label>
+  <div class="col-lg-6">
+  <select name="duration" id="duration" class="form-control">
+  <option value="">Select Duration in Month</option>
+  <option value="1">1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+  <option value="5">5</option>
+  <option value="6">6</option>
+  <option value="7">7</option>
+  <option value="8">8</option>
+  <option value="9">9</option>
+  <option value="10">10</option>
+  <option value="11">11</option>
+  <option value="12">12</option>
+  </select>
+  </div>
+  </div>
 
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Address : </label>
-    <div class="col-lg-8">
-    <textarea  rows="5" name="address"  id="address" class="form-control" required="required"></textarea>
-    </div>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label"><h4 style="Color: green" align="left">Personal info </h4> </label>
+  </div>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">City : </label>
-    <div class="col-lg-8">
-    <input type="text" name="city" id="city"  class="form-control" required="required">
-    </div>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Course:</label>
+  <div class="col-lg-6">
+  <select name="course" id="course" class="form-control" required>
+  <option value="">Select Course</option>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">State </label>
-    <div class="col-lg-8">
-    <select name="state" id="state"class="form-control" required>
-    <option value="">Select State</option>
+  </select> </div>
+  </div>
 
-    </select> </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Pincode : </label>
-    <div class="col-lg-8">
-    <input type="text" name="pincode" id="pincode"  class="form-control" required="required">
-    </div>
-    </div>
-
-    <div class="form-group">
-    <label class="col-sm-3 control-label"><h4 style="color: green" align="left">Permanent Address </h4> </label>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Registration No : </label>
+  <div class="col-lg-6">
+  <input type="text" name="regno" id="regno"  class="form-control" required="required" >
+  </div>
+  </div>
 
 
-    <div class="form-group">
-    <label class="col-sm-5 control-label">Permanent Address same as Correspondense address : </label>
-    <div class="col-sm-4">
-    <input type="checkbox" name="adcheck" value="1"/>
-    </div>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">First Name : </label>
+  <div class="col-lg-6">
+  <input type="text" name="fname" id="fname"  class="form-control" required="required" >
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Middle Name : </label>
+  <div class="col-lg-6">
+  <input type="text" name="mname" id="mname"  class="form-control">
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Last Name : </label>
+  <div class="col-lg-6">
+  <input type="text" name="lname" id="lname"  class="form-control" required="required">
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Gender : </label>
+  <div class="col-lg-6">
+  <select name="gender" class="form-control" required="required">
+  <option value="">Select Gender</option>
+  <option value="male">Male</option>
+  <option value="female">Female</option>
+  <option value="others">Others</option>
+  </select>
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Contact No : </label>
+  <div class="col-lg-6">
+  <input type="text" name="contact" id="contact"  class="form-control" required="required">
+  </div>
+  </div>
 
 
-    <div class="form-group">
-    <label class="col-lg-2 control-label">Address : </label>
-    <div class="col-lg-10">
-    <textarea  rows="5" name="paddress"  id="paddress" class="form-control" required="required"></textarea>
-    </div>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Email id : </label>
+  <div class="col-lg-6">
+  <input type="email" name="email" id="email"  class="form-control" required="required">
+  </div>
+  </div>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">City : </label>
-    <div class="col-lg-8">
-    <input type="text" name="pcity" id="pcity"  class="form-control" required="required">
-    </div>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Emergency Contact: </label>
+  <div class="col-lg-6">
+  <input type="text" name="econtact" id="econtact"  class="form-control" required="required">
+  </div>
+  </div>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">State : </label>
-    <div class="col-lg-8">
-    <select name="pstate" id="pstate"class="form-control" required>
-    <option value="">Select State</option>
-    </select> </div>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Guardian  Name : </label>
+  <div class="col-lg-6">
+  <input type="text" name="gname" id="gname"  class="form-control" required="required">
+  </div>
+  </div>
 
-    <div class="form-group">
-    <label class="col-lg-4 control-label">Pincode : </label>
-    <div class="col-lg-8">
-    <input type="text" name="ppincode" id="ppincode"  class="form-control" required="required">
-    </div>
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Guardian  Relation : </label>
+  <div class="col-lg-6">
+  <input type="text" name="grelation" id="grelation"  class="form-control" required="required">
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Guardian Contact no : </label>
+  <div class="col-lg-6">
+  <input type="text" name="gcontact" id="gcontact"  class="form-control" required="required">
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-sm-3 control-label"><h4 style="color: green" align="left">Correspondense Address </h4> </label>
+  </div>
 
 
-    <div class="col-sm-6 col-sm-offset-4">
-    <button class="btn btn-default" type="submit">Cancel</button>
-    <input type="submit" name="submit" Value="Register" class="btn btn-primary">
-    </div>
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Address : </label>
+  <div class="col-lg-6">
+  <textarea  rows="5" name="address"  id="address" class="form-control" required="required"></textarea>
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">City : </label>
+  <div class="col-lg-6">
+  <input type="text" name="city" id="city"  class="form-control" required="required">
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">State </label>
+  <div class="col-lg-6">
+  <select name="state" id="state"class="form-control" required>
+  <option value="">Select State</option>
+
+  </select> </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Pincode : </label>
+  <div class="col-lg-6">
+  <input type="text" name="pincode" id="pincode"  class="form-control" required="required">
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-sm-3 control-label"><h4 style="color: green" align="left">Permanent Address </h4> </label>
+  </div>
+
+
+  <div class="form-group">
+  <label class="col-sm-5 control-label">Permanent Address same as Correspondense address : </label>
+  <div class="col-sm-4">
+  <input type="checkbox" name="adcheck" value="1"/>
+  </div>
+  </div>
+
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Address : </label>
+  <div class="col-lg-6">
+  <textarea  rows="5" name="paddress"  id="paddress" class="form-control" required="required"></textarea>
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">City : </label>
+  <div class="col-lg-6">
+  <input type="text" name="pcity" id="pcity"  class="form-control" required="required">
+  </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">State : </label>
+  <div class="col-lg-6">
+  <select name="pstate" id="pstate"class="form-control" required>
+  <option value="">Select State</option>
+  </select> </div>
+  </div>
+
+  <div class="form-group">
+  <label class="col-lg-4 control-label">Pincode : </label>
+  <div class="col-lg-6">
+  <input type="text" name="ppincode" id="ppincode"  class="form-control" required="required">
+  </div>
+  </div>
+
+
+  <div class="col-sm-6 col-sm-offset-4">
+  <button class="btn btn-default" type="submit">Cancel</button>
+  <input type="submit" name="submit" Value="Register" class="btn btn-primary">
+  </div>
 </form>
 
-
-
-
-
-	        </div>
-
+</div>
 	<!-- /.container-fluid -->
 </div>
 <?php include "footer.php" ?>
