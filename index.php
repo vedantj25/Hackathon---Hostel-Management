8671d0d12de9b5426d5a959846fba30d1c00d56a
vedantj25 <vjjc25@gmail.com>
2020-07-17 10:33:07 +0530

    Create Files

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>Hello!</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+  <div class="card">
+    <div class="card-body">
+      <p>Hello !</p>
+    </div>
+  </div>
+</body>
+</html>
